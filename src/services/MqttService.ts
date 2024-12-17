c6f4be8d25212ae37a3024bdde5024a530ede906
Irvan Rizki Nugraha <irvanriskinugraha@90@gmail.com>
2024-12-17 19:12:20 +0700

    change try catch to then catch

--- src/services/MqttService.ts
+++ src/services/MqttService.ts
@@ -2,36 +2,38 @@ import axios from "axios";
 import * as mqtt from "mqtt";
 
 export class MqttService {
-	static mqttClient: mqtt.MqttClient;
-	static connect() {
-		MqttService.mqttClient = mqtt.connect(
-			process.env.MQTT_BROKER || 'mqtt://broker.emqx.io', {
-				clientId: 'mqttjs_' + Math.random().toString(16).substring(2, 8),
-				reconnectPeriod: 5000,
-				clean: true,
-				username: process.env.MQTT_USERNAME || '',
-				password: process.env.MQTT_PASSWORD || '',
-			}
-		);
+    static mqttClient: mqtt.MqttClient;
+    static connect() {
+        MqttService.mqttClient = mqtt.connect(
+            process.env.MQTT_BROKER || 'mqtt://broker.emqx.io', {
+                clientId: 'mqttjs_' + Math.random().toString(16).substring(2, 8),
+                reconnectPeriod: 5000,
+                clean: true,
+                username: process.env.MQTT_USERNAME || '',
+                password: process.env.MQTT_PASSWORD || '',
+            }
+        );
 
-		MqttService.mqttClient.on('connect', () => {
-			console.log('Connected to MQTT Broker');
-		});
+        MqttService.mqttClient.on('connect', () => {
+            console.log('Connected to MQTT Broker');
+        });
 
-		for (let i = 1; i <= 94; i++) {
-			MqttService.mqttClient.subscribe(`data${i}`);
-			console.log(`Subscribed to data${i}`);
-		}
+        for (let i = 1; i <= 94; i++) {
+            MqttService.mqttClient.subscribe(`data${i}`);
+            console.log(`Subscribed to data${i}`);
+        }
 
-		MqttService.mqttClient.on('message', (topic, message) => {
-			try {
-				axios.post(process.env.BASE_URL || 'https://reqres.in/api/users', {
-					topic,
-					value: message.toString(),
-				});
-			} catch (error) {
-				console.error(error);
-			}
-		});
-	}
+        MqttService.mqttClient.on('message', (topic, message) => {
+            axios.post(process.env.BASE_URL || 'https://reqres.in/api/users', {
+                topic,
+                value: message.toString(),
+            })
+            .then(response => {
+                console.log(`Message sent successfully: ${response.data}`);
+            })
+            .catch(error => {
+                console.error(`Error sending message: ${error}`);
+            });
+        });
+    }
 }
